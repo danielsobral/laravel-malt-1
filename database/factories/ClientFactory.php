6cfbbad254a9a096cd486f402293100dbf86d161
Daniel Sobral do Nascimento <daniel.nascimento@rainforest.tech>
2023-12-18 20:12:57 -0300

    Mais sobre Factories

---
+++ database/factories/ClientFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Client>
+ */
+class ClientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'uuid' => Str::orderedUuid(),
+            'user_id' => User::factory(),
+            'document' => fake('pt_BR')->cpf,
+            'birthdate' => fake()->dateTimeThisCentury()
+        ];
+    }
+}
