7d8965a4826d0dc174ea09922181f4fb72d5be8a
Daniel Sobral do Nascimento <daniel.nascimento@rainforest.tech>
2023-12-19 20:00:25 -0300

    Mais sobre models: Campos customizados

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -20,5 +21,27 @@ class Plan extends Model
     public function signatures()
     {
         return $this->hasMany(Signature::class);
+    }
+
+    public function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn($name) => ucfirst($name)
+        );
+    }
+
+    public function cod(): Attribute
+    {
+        return Attribute::make(
+            get: fn($cod) => strtoupper($cod),
+            set: fn($cod) => strtolower($cod)
+        );
+    }
+
+    public function fullname(): Attribute
+    {
+        return Attribute::make(
+            get: fn($name, $attributes) => strtoupper($attributes['name']) . '_' . strtoupper($attributes['cod'])
+        );
     }
 }
